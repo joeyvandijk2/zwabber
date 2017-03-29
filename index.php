c5143d5694adb586124a0234758f9e91d6e91fd1
sjihdazi <sjihdazihellingman@hotmail.com>
2017-03-29 13:14:37 +0200

    modal test
    modal test

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include_once("classes/table.php");
 include_once("classes/database.php");
 $classtable = new table();
@@ -12,6 +13,35 @@ if($_GET["table"] == "kamer"){
 if($_GET["table"] == "object"){
     $table = $classtable->objecten();
 }
+
+$db = new db();
+$bedrijven = $db->conn->query($db->select("bedrijf"));
+    while($row = $result->fetch_assoc())
+    {
+    echo '<!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+      Launch demo modal
+    </button>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+          </div>
+          <div class="modal-body">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>';
+    }
+
 
 ?>
 
