21982b8baf7d6158b5d9f225cb2ff04ad937ee70
sjihdazi <sjihdazihellingman@hotmail.com>
2017-03-30 13:07:19 +0200

    modals
    modals

--- index.php
+++ index.php
@@ -2,23 +2,17 @@
 session_start();
 include_once("classes/table.php");
 include_once("classes/database.php");
+
+$db = new db();
 $classtable = new table();
+
 if(empty($_GET["table"])){$_GET["table"] = "bedrijf";}
 if($_GET["table"] == "bedrijf"){
     $table = $classtable->bedrijfen();
-}
-if($_GET["table"] == "kamer"){
-    $table = $classtable->kamers();
-}
-if($_GET["table"] == "object"){
-    $table = $classtable->objecten();
-}
-
-    $db = new db();
     $bedrijven = $db->conn->query($db->select("bedrijf"));
     while($row = $bedrijven->fetch_assoc())
     {
-    echo '
+        echo '
     <!-- Small modal -->
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".upd'.$row["bedrijfId"].'">Small modal</button>
     
@@ -62,6 +56,106 @@ if($_GET["table"] == "object"){
       </div>
     </div>';
     }
+}
+if($_GET["table"] == "kamer") {
+    $table = $classtable->kamers();
+    $kamers = $db->conn->query($db->select("kamer"));
+    while ($row = $kamers->fetch_assoc()) {
+        echo '
+    <!-- Small modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".upd' . $row["kamerId"] . '">Small modal</button>
+    
+    <div class="modal fade bs-example-modal-sm upd' . $row["kamerId"] . '" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+      <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+          </div>
+          <div class="modal-body">
+            update kamer ' . $row["kamerId"] . '?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>';
+
+        echo '
+    <!-- Small modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".del' . $row["kamerId"] . '">Small modal</button>
+    
+    <div class="modal fade bs-example-modal-sm del' . $row["kamerId"] . '" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+      <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+          </div>
+          <div class="modal-body">
+            dlete >?' . $row["kamerId"] . '
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>';
+    }
+}
+if($_GET["table"] == "object") {
+    $table = $classtable->objecten();
+    $object = $db->conn->query($db->select("object"));
+    while ($row = $object->fetch_assoc()) {
+        echo '
+    <!-- Small modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".upd' . $row["objectId"] . '">Small modal</button>
+    
+    <div class="modal fade bs-example-modal-sm upd' . $row["objectId"] . '" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+      <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+          </div>
+          <div class="modal-body">
+            update object ' . $row["objectId"] . '?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>';
+
+        echo '
+    <!-- Small modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".del' . $row["objectId"] . '">Small modal</button>
+    
+    <div class="modal fade bs-example-modal-sm del' . $row["objectId"] . '" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+      <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+          </div>
+          <div class="modal-body">
+            dlete >?' . $row["objectId"] . '
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>';
+    }
+}
+
 
 
 ?>
